bd5f7321c898e91b3ef5659ee892438a1e56fc31
George Styles <george@ripnet.co.uk>
2023-01-15 00:35:49 +0000

    moved clock

--- config/ripnetuk-components/components/ripnetuk_neopixel/__init__.py
+++ config/ripnetuk-components/components/ripnetuk_neopixel/__init__.py
@@ -2,26 +2,18 @@ import esphome.config_validation as cv
 import esphome.codegen as cg
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ['logger']
+DEPENDENCIES = ['logger', 'sensor']
+AUTO_LOAD = ['ripnetuk_ui', 'sensor']
 
-debug_ns = cg.esphome_ns.namespace('ripnetuk_neopixel')
+ns = cg.esphome_ns.namespace('ripnetuk_neopixel')
 
-RipnetUkNeopixelComponent = debug_ns.class_(
-    'RipnetUkNeopixelComponent', cg.Component)
+RipnetUkNeopixelComponent = ns.class_('RipnetUkNeopixelComponent', cg.Component)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(RipnetUkNeopixelComponent),
+})
 
-CONF_TEST = "test"
-
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(RipnetUkNeopixelComponent),
-            cv.Required(CONF_TEST): cv.string_strict
-        }
-    )
-    .extend(cv.COMPONENT_SCHEMA)
-    .extend(cv.polling_component_schema('1min'))
-)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
+    
