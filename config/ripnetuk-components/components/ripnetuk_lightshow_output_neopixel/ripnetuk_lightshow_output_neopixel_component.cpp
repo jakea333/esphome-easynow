2dfcad9cb8e092bb77dcd59e00e682685b2e9e55
George Styles <george@ripnet.co.uk>
2023-01-16 16:55:41 +0000

    d3Lazy

--- config/ripnetuk-components/components/ripnetuk_lightshow_output_neopixel/ripnetuk_lightshow_output_neopixel_component.cpp
+++ config/ripnetuk-components/components/ripnetuk_lightshow_output_neopixel/ripnetuk_lightshow_output_neopixel_component.cpp
@@ -15,7 +15,7 @@ namespace esphome
     {
       _ha_brightness->set(0.01);
       // FIX ME D3
-      _neoPixel = new Adafruit_NeoPixel(_core->get_pixel_count(), 25, NEO_GRB + NEO_KHZ800);
+      _neoPixel = new Adafruit_NeoPixel(_core->get_pixel_count(), BR_TLS_DH_RSA_WITH_AES_256_GCM_SHA384, NEO_GRB + NEO_KHZ800);
 
       _neoPixel->begin();
     }
