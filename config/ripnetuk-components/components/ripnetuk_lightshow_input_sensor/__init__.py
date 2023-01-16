afa1db1dcc063bba5715c183e2e23b3546ff3072
George Styles <george@ripnet.co.uk>
2023-01-16 23:37:16 +0000

    Lazy

--- config/ripnetuk-components/components/ripnetuk_lightshow_input_sensor/__init__.py
+++ config/ripnetuk-components/components/ripnetuk_lightshow_input_sensor/__init__.py
@@ -13,17 +13,19 @@ coreclass = corens.class_('RipnetUkLightshowCoreComponent', cg.Component)
 
 ns = cg.esphome_ns.namespace('ripnetuk_lightshow_input_sensor')
 
-RipnetUkLightshowInputSensorComponent = ns.class_(
-    'RipnetUkLightshowInputSensorComponent', cg.Component)
+BaseRipnetUkLightshowInputComponent = corens.class_('BaseRipnetUkLightshowInputComponent', cg.Component)
 
-CONF_LIGHTSHOW_CORE = "lightshow_core"
+RipnetUkLightshowInputSensorComponent = ns.class_(
+    'RipnetUkLightshowInputSensorComponent', cg.Component, BaseRipnetUkLightshowInputComponent)
+
+# CONF_LIGHTSHOW_CORE = "lightshow_core"
 CONF_SENSOR = "sensor"
 
 CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(RipnetUkLightshowInputSensorComponent),
-            cv.Required(CONF_LIGHTSHOW_CORE): cv.use_id(coreclass),
+            # cv.Required(CONF_LIGHTSHOW_CORE): cv.use_id(coreclass),
             cv.Required(CONF_SENSOR): cv.use_id(sensor.Sensor),
         }
     )
@@ -35,8 +37,8 @@ def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
 
-    lightshow_core = yield cg.get_variable(config[CONF_LIGHTSHOW_CORE])
-    cg.add(var.set_lightshow_core(lightshow_core))
+    # lightshow_core = yield cg.get_variable(config[CONF_LIGHTSHOW_CORE])
+    # cg.add(var.set_lightshow_core(lightshow_core))
 
     sensor = yield cg.get_variable(config[CONF_SENSOR])
     cg.add(var.set_sensor(sensor))
