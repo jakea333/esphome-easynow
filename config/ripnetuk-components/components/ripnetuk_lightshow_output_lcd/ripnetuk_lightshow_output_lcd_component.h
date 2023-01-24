0e93c48208f61a5fcf571fc90aa959d341f1843b
George Styles <george@ripnet.co.uk>
2023-01-24 15:35:30 +0000

    Lazy

--- config/ripnetuk-components/components/ripnetuk_lightshow_output_lcd/ripnetuk_lightshow_output_lcd_component.h
+++ config/ripnetuk-components/components/ripnetuk_lightshow_output_lcd/ripnetuk_lightshow_output_lcd_component.h
@@ -4,18 +4,19 @@
 #include "../ripnetuk_lightshow_core/frame.h"
 #include "../ripnetuk_ui/ripnetuk_switch_component.h"
 #include "../ripnetuk_ui/ripnetuk_button_component.h"
+#include "../ili9341/ili9341_display.h"
 
 namespace esphome
 {
   namespace ripnetuk_lightshow_output_lcd
   {
-    class RipnetUkLightshowOutputLcdComponent : public Component, public ripnetuk_lightshow_core::BaseRipnetUkLightshowOutputComponent
+    class RipnetUkLightshowOutputLcdComponent : public ili9341::ILI9341TFT24, public ripnetuk_lightshow_core::BaseRipnetUkLightshowOutputComponent
     {
     public:
       void setup() override;
       void output_frame(ripnetuk_lightshow_core::Frame *frame);
       float get_setup_priority() const override;
-      
+
     private:
       ripnetuk_ui::RipnetUkSwitchComponent *_ha_test = new ripnetuk_ui::RipnetUkSwitchComponent("Test");
       ripnetuk_ui::RipnetUkButtonComponent *_ha_clock_reset_button = new ripnetuk_ui::RipnetUkButtonComponent("Run Test", [this]
