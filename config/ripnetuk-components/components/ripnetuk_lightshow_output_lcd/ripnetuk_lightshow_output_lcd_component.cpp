9ec1e7319d1882eb0a9af3026a2fa6797c6a411f
George Styles <george@ripnet.co.uk>
2023-01-24 14:50:48 +0000

    Lazy

--- config/ripnetuk-components/components/ripnetuk_lightshow_output_lcd/ripnetuk_lightshow_output_lcd_component.cpp
+++ config/ripnetuk-components/components/ripnetuk_lightshow_output_lcd/ripnetuk_lightshow_output_lcd_component.cpp
@@ -2,20 +2,20 @@
 #include "esphome/core/log.h"
 #include "esphome.h"
 
-#include "SPI.h"
-#include <Adafruit_GFX.h>
-#include <Adafruit_ILI9341.h>
+// #include "SPI.h"
+// #include <Adafruit_GFX.h>
+// #include <Adafruit_ILI9341.h>
 
-#define TFT_DC 21
-#define TFT_CS 22
-#define TFT_MOSI 23
-#define TFT_CLK 19
-#define TFT_RST 18
-#define TFT_MISO 13
+// #define TFT_DC 21
+// #define TFT_CS 22
+// #define TFT_MOSI 23
+// #define TFT_CLK 19
+// #define TFT_RST 18
+// #define TFT_MISO 13
 
 // Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 //  If using the breakout, change pins as desired
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+// Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
 namespace esphome
 {
@@ -25,7 +25,7 @@ namespace esphome
 
     void RipnetUkLightshowOutputLcdComponent::setup()
     {
-      tft.begin();
+      // tft.begin();
       _ha_test->set(false);
     }
 
@@ -41,7 +41,7 @@ namespace esphome
 
     void  RipnetUkLightshowOutputLcdComponent::run_test()
     {
-        tft.fillScreen(millis() % 0xFFFF);
+        // tft.fillScreen(millis() % 0xFFFF);
     }
   }
 }
