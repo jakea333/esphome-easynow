fc17abe05b11b26825e46c892b27ce9964587a80
George Styles <george@ripnet.co.uk>
2023-01-31 00:29:43 +0000

    mac

--- config/ripnetuk-components/components/proxy_receiver/proxy_receiver.cpp
+++ config/ripnetuk-components/components/proxy_receiver/proxy_receiver.cpp
@@ -13,8 +13,12 @@ namespace esphome
     void ProxyReceiverComponent::loop()
     {
       proxy_base::ProxyPayload pl;
-
-      ESP_LOGD(TAG, "Time %d magic %d", millis(), pl.get_magic_number());
+      ESP_LOGD(TAG, "%s Time %d magic %d", WiFi.macAddress().c_str(), millis(), pl.get_magic_number());
+      uint8_t mac[6];
+      esp_wifi_get_mac(WIFI_IF_STA, mac);
+      char mac_str[18];
+      sprintf(mac_str, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+      ESP_LOGD("my_component", "MAC address: %s", mac_str);
     }
 
     float ProxyReceiverComponent::get_setup_priority() const
