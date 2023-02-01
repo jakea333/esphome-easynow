0d1521a665ae1d53d6d2e55f791ed9e5b62851ca
George Styles <george@ripnet.co.uk>
2023-02-01 00:26:59 +0000

    Lazy

--- config/ripnetuk-components/components/proxy_base/proxy_base.cpp
+++ config/ripnetuk-components/components/proxy_base/proxy_base.cpp
@@ -2,6 +2,8 @@
 #include "esphome/core/log.h"
 #include "esphome/core/hal.h"
 #include <WiFi.h>
+#include "esphome/core/application.h"
+#include <stdio.h>
 
 #define PROXY_LOG_PERIOD 60000
 
@@ -9,7 +11,16 @@ namespace esphome
 {
   namespace proxy_base
   {
-    static const char *TAG = "ProxyBaseComponent";
+    char *tag_ = NULL;
+    char *get_tag()
+    {
+      if (tag_ == NULL)
+      {
+        tag_ = (char *)malloc(100);
+        sprintf(tag_, "[%s] ProxyBaseComponent", App.get_name().c_str());
+      }
+      return tag_;
+    }
 
     std::vector<ProxyBaseComponent *> *ProxyBaseComponent::callback_component_list_ = new std::vector<ProxyBaseComponent *>();
 
@@ -20,15 +31,15 @@ namespace esphome
 
     void ProxyBaseComponent::ProxyBaseComponent::OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
     {
-      ESP_LOGD(TAG, "Last Packet Send Status:");
-      ESP_LOGD(TAG, (status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail"));
+      ESP_LOGD(get_tag(), "%s Last Packet Send Status:", App.get_name().c_str());
+      ESP_LOGD(get_tag(), (status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail"));
     }
 
     void ProxyBaseComponent::OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
     {
       proxy_message message;
       memcpy(&message, incomingData, sizeof(message));
-      ESP_LOGD(TAG, "Bytes received: %d messge_type %d", len, message.message_type);
+      ESP_LOGD(get_tag(), "Bytes received: %d messge_type %d", len, message.message_type);
       for (int i = 0; i < callback_component_list_->size(); i++)
       {
         callback_component_list_->at(i)->handle_received_proxy_message(mac_addr, &message);
@@ -40,7 +51,7 @@ namespace esphome
       if ((millis() - last_log_millis_) > PROXY_LOG_PERIOD)
       {
         last_log_millis_ = millis();
-        ESP_LOGD(TAG, "%s Time %d ", WiFi.macAddress().c_str(), millis());
+        ESP_LOGD(get_tag(), "%s Time %d ", WiFi.macAddress().c_str(), millis());
       }
     }
 
@@ -53,15 +64,15 @@ namespace esphome
     {
       espnow_is_setup_ = false;
 
-      ESP_LOGD(TAG, "Setting WiFi mode");
+      ESP_LOGD(get_tag(), "Setting WiFi mode");
       WiFi.mode(WIFI_AP_STA);
-      ESP_LOGD(TAG, "WiFi mode set");
+      ESP_LOGD(get_tag(), "WiFi mode set");
       if (esp_now_init() != ESP_OK)
       {
-        ESP_LOGD(TAG, "Error initializing ESP-NOW");
+        ESP_LOGD(get_tag(), "Error initializing ESP-NOW");
         return false;
       }
-      ESP_LOGD(TAG, "ESP-NOW Initialized");
+      ESP_LOGD(get_tag(), "ESP-NOW Initialized");
 
       esp_now_register_send_cb(OnDataSent);
       esp_now_register_recv_cb(OnDataRecv);
@@ -82,15 +93,16 @@ namespace esphome
       }
       // memcpy(peerInfo.peer_addr, peer_mac_address, 6);
 
-      ESP_LOGD(TAG, "Add peer %02X:%02X:%02X:%02X:%02X:%02X", peerInfo.peer_addr[0], peerInfo.peer_addr[1], peerInfo.peer_addr[2], peerInfo.peer_addr[3], peerInfo.peer_addr[4], peerInfo.peer_addr[5]);
+      ESP_LOGD(get_tag(), "Add peer %02X:%02X:%02X:%02X:%02X:%02X", peerInfo.peer_addr[0], peerInfo.peer_addr[1], peerInfo.peer_addr[2], peerInfo.peer_addr[3], peerInfo.peer_addr[4], peerInfo.peer_addr[5]);
+      
       peerInfo.channel = 11;
       peerInfo.encrypt = false;
 
       if (esp_now_add_peer(&peerInfo) != ESP_OK)
       {
-        ESP_LOGD(TAG, "Failed to add peer");
+        ESP_LOGD(get_tag(), "Failed to add peer");
         return false;
-        ESP_LOGD(TAG, "Peer Added");
+        ESP_LOGD(get_tag(), "Peer Added");
       }
 
       return true;
@@ -100,17 +112,17 @@ namespace esphome
     {
       if (!espnow_is_setup_)
       {
-        ESP_LOGD(TAG, "Send Proxy Message Called when ESPNow if not setup.");
+        ESP_LOGD(get_tag(), "Send Proxy Message Called when ESPNow if not setup.");
         return false;
       }
       esp_err_t result = esp_now_send(peerInfo.peer_addr, (uint8_t *)message, sizeof(*message));
 
       if (result != ESP_OK)
       {
-        ESP_LOGD(TAG, "Send failed");
+        ESP_LOGD(get_tag(), "Send failed");
         return false;
       }
-      ESP_LOGD(TAG, "Sent with success");
+      ESP_LOGD(get_tag(), "Sent with success");
       return true;
     }
   } // namespace proxy_base
