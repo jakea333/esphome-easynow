9ff44c68053add7464393156b6ece9c1fda48455
George Styles <george@ripnet.co.uk>
2023-02-01 13:00:34 +0000

    Lazy

--- config/ripnetuk-components/components/proxy_transmitter/peer_receiver.cpp
+++ config/ripnetuk-components/components/proxy_transmitter/peer_receiver.cpp
@@ -2,7 +2,7 @@
 #include "esphome/core/log.h"
 #include "esphome/core/hal.h"
 #include <WiFi.h>
-#include "../proxy_base/log_tag.h"
+
 
 // uint8_t peer_address[] = {0x34, 0x86, 0x5D, 0xFC, 0x5E, 0xE0};
 
@@ -11,6 +11,6 @@ namespace esphome
   namespace proxy_transmitter
   {
 
-    proxy_base::LogTag *TAG = new proxy_base::LogTag("PeerReceiver");
+ 
   } // namespace proxy_receiver
 } // namespace esphome
