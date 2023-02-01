2ddba7030b6c67ec3b6bd6132ce05d132b011570
George Styles <george@ripnet.co.uk>
2023-02-01 21:36:37 +0000

    Lazy

--- config/ripnetuk-components/components/proxy_transmitter/__init__.py
+++ config/ripnetuk-components/components/proxy_transmitter/__init__.py
@@ -1,12 +1,22 @@
 import esphome.config_validation as cv
 import esphome.codegen as cg
 from esphome.const import CONF_ID
+from esphome.components import sensor
 
 DEPENDENCIES = ['logger', 'proxy_base']
 AUTO_LOAD = ['proxy_base']
 
 CONF_ESPNOW_CHANNEL = "espnow_channel"
 CONF_RECEIVER_MAC_ADDRESS = "receiver_mac_address"
+CONF_SENSORS = "sensors"
+
+CONF_SENSOR = "sensor"
+
+
+SENSOR_SCHEMA = cv.Schema({
+    cv.Required(CONF_SENSOR): cv.use_id(sensor.Sensor),
+})
+
 
 ns = cg.esphome_ns.namespace('proxy_transmitter')
 ProxyTransmitterComponent = ns.class_(
@@ -15,7 +25,8 @@ ProxyTransmitterComponent = ns.class_(
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ProxyTransmitterComponent),
     cv.Required(CONF_ESPNOW_CHANNEL): cv.int_,
-    cv.Required(CONF_RECEIVER_MAC_ADDRESS): cv.mac_address
+    cv.Required(CONF_RECEIVER_MAC_ADDRESS): cv.mac_address,
+    cv.Required(CONF_SENSORS): cv.ensure_list(SENSOR_SCHEMA),
 })
 
 
@@ -26,3 +37,7 @@ def to_code(config):
     cg.add(var.set_espnow_channel(config[CONF_ESPNOW_CHANNEL]))
     cg.add(var.set_receiver_mac_address(
         config[CONF_RECEIVER_MAC_ADDRESS].as_hex))
+
+    for sensor_config in config.get(CONF_SENSORS, []):
+        sensor = yield cg.get_variable(sensor_config[CONF_SENSOR])
+        cg.add(var.add_sensor(sensor))
--- config/ripnetuk-components/components/proxy_transmitter/proxy_transmitter.h
+++ config/ripnetuk-components/components/proxy_transmitter/proxy_transmitter.h
@@ -4,6 +4,8 @@
 #include "../proxy_base/proxy_base.h"
 #include "peer_receiver.h"
 #include "../proxy_base/log_tag.h"
+#include <vector>
+#include "esphome/components/sensor/sensor.h"
 
 namespace esphome
 {
@@ -13,16 +15,16 @@ namespace esphome
     class ProxyTransmitterComponent : public proxy_base::ProxyBaseComponent
     {
     private:
-
     protected:
       PeerReceiver *peer_receiver_ = new PeerReceiver();
 
     public:
+      vector<sensor::Sensor *> sensors = new vector<sensor::Sensor>();
       void loop() override;
       void setup() override;
       void set_espnow_channel(int channel) { espnow_channel_ = channel; }
       void set_receiver_mac_address(uint64_t mac_address) { peer_receiver_->mac_address.set_from_uint64_t(mac_address); };
-    };
+      void add_sensor(sensor::Sensor *sensor) { sensors->push_back(sensor); };
 
-  } // namespace proxy_transmitter
-} // namespace esphome
+    } // namespace proxy_transmitter
+  }   // namespace esphome
