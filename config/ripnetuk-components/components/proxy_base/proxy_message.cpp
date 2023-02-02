b73710d0d2df322d42427c1bbfede53ba42390b6
George Styles <george@ripnet.co.uk>
2023-02-02 17:30:15 +0000

    Lazy

--- config/ripnetuk-components/components/proxy_base/proxy_message.cpp
+++ config/ripnetuk-components/components/proxy_base/proxy_message.cpp
@@ -9,25 +9,25 @@ namespace esphome
             switch (message->message_type)
             {
             case T_TO_R_CHECKIN:
-                output->append("T to R Check in");
+                output->append("T->R Check in");
                 break;
             case R_TO_T_CHECKIN_RESP:
             {
                 std::string enter_ota_mode = std::to_string(message->checkin_response.enter_ota_mode);
-                output->append("R to C Check in response - enter OTA mode " + enter_ota_mode);
+                output->append("R->T Check in response - enter OTA mode " + enter_ota_mode);
                 break;
             }
             case T_TO_R_SEND_SENSOR_STATE:
             {
                 std::string state = std::to_string(message->send_sensor_state.state);
                 std::string proxy_id = std::string(message->send_sensor_state.proxy_id);
-                output->append("T to R Send Sensor State (" + proxy_id + ") State - " + state);
+                output->append("T->R Send Sensor State (" + proxy_id + ") State - " + state);
                 break;
             }
             case R_TO_T_SEND_SENDOR_STATE_REPONSE:
             {
                 std::string proxy_id = std::string(message->send_sensor_state.proxy_id);
-                output->append("R to C Send Sensor State Response (" + proxy_id + ")");
+                output->append("R->T Send Sensor State Response (" + proxy_id + ")");
                 break;
             }
             default:
