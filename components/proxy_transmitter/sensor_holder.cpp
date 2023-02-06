7eda26088c8a071bd1aba684f92d1f18aaab17f0
George Styles <george@ripnet.co.uk>
2023-02-06 00:26:48 +0000

    Only consider a sensor read if its state != NAN

--- components/proxy_transmitter/sensor_holder.cpp
+++ components/proxy_transmitter/sensor_holder.cpp
@@ -1,5 +1,6 @@
 #include "sensor_holder.h"
 #include "esphome/core/log.h"
+#include <math.h>
 
 namespace esphome
 {
@@ -15,8 +16,11 @@ namespace esphome
 
     void SensorHolder::on_state_callback(float state)
     {
-      has_state = true;
-      this->state = state;
+      if (!isnan(state))
+      {
+        has_state = true;
+        this->state = state;
+      }
     }
   } // namespace proxy_transmitter
 } // namespace esphome
